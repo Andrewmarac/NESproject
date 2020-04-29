d306d69a9984f98f901356ba2d06f1230679b470
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2020-04-29 16:53:01 +0000

    1 commit

---
+++ relab-server/models/feature.model.js
@@ -0,0 +1,15 @@
+module.exports = class Feature{
+ constructor(id, geometry) {
+ this.type = "Feature";
+ this.properties = new Properties(id); //Per ora le proprietà contengono un solo valore (id)
+ this.geometry = geometry; //Contiene la geometria del poligono.
+ }
+}
+//Iniziamo a preparare la classe Properties che complicheremo in seguito
+class Properties
+{
+ constructor(id)
+ {
+ this.id = id;
+ }
+}
